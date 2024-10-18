94dce7f88295a924c514a2af611dac7bf81c0288
Eric Hester <eric.hester@umbrella.tech>
2024-10-17 23:14:54 -0400

    .

--- netbox_certificate_plugin/views.py
+++ netbox_certificate_plugin/views.py
@@ -42,7 +42,7 @@ def fetch_certificate(request):
 
         # Determine the certificate type
         type = 'standard'  # Default to 'standard'
-        if '*' in san_names:
+        if '*' in san_names[2]:
             wildcard = True
             type = 'wildcard'
         elif len(san_names) > 1:
